57420e29ab65bf358f19bd9d8ae5a9eed1967b31
wasdopit <wasdopit@outlook.com>
2025-10-02 23:51:18 +0100

    Add files via upload

--- macbeth.js
+++ macbeth.js
@@ -1,19 +1,10 @@
 document.addEventListener("click", function(event) {
-  const marks = document.querySelectorAll("mark");
+  // close all notes
+  document.querySelectorAll("mark").forEach(m => m.classList.remove("active"));
 
-  // Close all open tooltips
-  marks.forEach(m => m.classList.remove("active"));
-
-  // If you clicked a <mark>, toggle it
+  // if you clicked a mark, toggle it
   if (event.target.tagName === "MARK") {
     event.target.classList.add("active");
     event.stopPropagation();
   }
-});
-
-// Prevent tooltips from closing if you click inside the tooltip text itself
-document.querySelectorAll("mark").forEach(mark => {
-  mark.addEventListener("click", function(e) {
-    e.stopPropagation();
-  });
 });
